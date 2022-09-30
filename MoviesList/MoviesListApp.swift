291441c5c51441b1dde063803a886b2fde4ce01c
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2022-09-30 22:53:48 +0100

    Initial Commit

---
+++ MoviesList/MoviesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesListApp.swift
+//  MoviesList
+//
+//  Created by Bouchedoub Rmazi on 30/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
